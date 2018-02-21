eb11612e1e384b259353aadd4b4e662d18fc646b
AlliterativeAnchovies <33570983+AlliterativeAnchovies@users.noreply.github.com>
2018-02-21 11:00:31 +0000

    Added support for all systems

--- comedian.py
+++ comedian.py
@@ -1,7 +1,39 @@
-import pygame
-import pygame.camera
-from pygame.locals import *
+#import pygame
+#import pygame.camera
+#from pygame.locals import *
+import numpy as np
+import cv2
 
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+	
+    if ret == False: break
+	
+    #crop frame to square
+    starty = (len(frame))//2-240
+    startx = len(frame[0])//2-240
+    frame = frame[starty:starty+480, startx:startx+480]
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    #save it
+    cv2.imwrite("output.png",frame)
+    break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+"""
 DEVICE = '/dev/video0'
 SIZE = (480, 480)
 FILENAME = 'capture.png'
@@ -16,7 +48,7 @@ def camStream():
     capture = True
     while capture:
         screen = camera.get_image()
-        #screen = pygame.transform.scale(screen, (480,480))
+        screen = pygame.transform.scale(screen, (480,480))
         display.blit(screen, (0,0))
         pygame.display.flip()
         for event in pygame.event.get():
@@ -30,3 +62,4 @@ def camStream():
 
 
 camStream()
+"""
